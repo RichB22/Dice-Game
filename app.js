e2261638297350cfdcf724e2ce22337a72ab786e
Richard Burner <60147649+RichB22@users.noreply.github.com>
2020-06-30 14:22:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.querySelector('.btn-hold').addEventListener('click',function() { if (ga
 
 
     // Check if plpayer WON the Game 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
         document.querySelector('#name-' + activePlayer).textContent = 'WINNER!!!'; 
         document.querySelector('.dice').style.display = 'none'; 
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
